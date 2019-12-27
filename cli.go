011954715db1f62cc0b4cacc92841b31f415047e
Aibek <aibek.abdykasymov@gmail.com>
2019-12-27 18:40:23 +0600

    Added addresses and wallets functionality

--- cli.go
+++ cli.go
@@ -5,21 +5,16 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 )
 
 type CLI struct {}
 
-func (cli *CLI) createBlockChain(address string) {
-	bc := CreateBlockchain(address)
-	bc.db.Close()
-	fmt.Println("Done!")
-}
-
 func (cli *CLI) printUsage() {
 	fmt.Println("Usage: ")
-	fmt.Println(" getbalance -address ADDRESS - Get balance of ADDRESS")
 	fmt.Println(" createblockchain -address ADDRESS - Create a blockchain and send genesis block reward to ADDRESS")
+	fmt.Println(" create wallet - Generates a new key-pair and saves it into the wallet file")
+	fmt.Println(" getbalance -address ADDRESS - Get balance of ADDRESS")
+	fmt.Println(" listaddresses - Lists all addresses from the wallet file")
 	fmt.Println(" printchain - Print all the blocks of the blockchain")
 	fmt.Println(" send -from FROM -to TO -amount AMOUNT - Send AMOUNT of coins from FROM address to TO")
 }
@@ -31,50 +26,14 @@ func (cli *CLI) validateArgs() {
 	}
 }
 
-func (cli *CLI) send(from, to string, amount int) {
-	bc := NewBlockchain(from)
-	defer bc.db.Close()
-	tx := NewUTXOTransaction(from, to, amount, bc)
-	bc.MineBlock([]*Transaction{tx})
-	fmt.Println("Success!")
-}
-
-func (cli *CLI) printChain() {
-	bc := NewBlockchain((""))
-	defer bc.db.Close()
-	bci := bc.Iterator()
-	for {
-		block := bci.Next()
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
-}
-
-func (cli *CLI) getBalance(address string) {
-	bc := NewBlockchain(address)
-	defer bc.db.Close()
-	balance := 0
-	UTXOs := bc.FindUTXO(address)
-	for _ ,out := range UTXOs {
-		balance += out.Value
-	}
-	fmt.Printf("Balance of '%s': %d\n", address, balance)
-}
-
-
-
 func (cli *CLI) Run() {
 	cli.validateArgs()
-	getBalanceCmd := flag.NewFlagSet("getbalance", flag.ExitOnError)
 	createBlockchainCmd := flag.NewFlagSet("createblockchain", flag.ExitOnError)
-	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
+	createWalletCmd := flag.NewFlagSet("createwallet", flag.ExitOnError)
+	getBalanceCmd := flag.NewFlagSet("getbalance", flag.ExitOnError)
+	listAddressesCmd := flag.NewFlagSet("listaddresses", flag.ExitOnError)
 	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
 
 	getBalanceAddress := getBalanceCmd.String("address", "", "The address to get balace for")
 	createBlockchainAddress := createBlockchainCmd.String("address", "", "The address to send genesis block reward to")
@@ -82,24 +41,34 @@ func (cli *CLI) Run() {
 	sendTo := sendCmd.String("to", "", "Destination walled address")
 	sendAmount := sendCmd.Int("amount", 0, "Amount to send")
 
-	switch os.Args[1] {
-	case "getbalance":
-		err := getBalanceCmd.Parse(os.Args[2:])
-		if err != nil {
-			log.Panic(err)
-		}
+	switch os.Args[1] {	
 	case "createblockchain":
 		err := createBlockchainCmd.Parse(os.Args[2:])
 		if err != nil {
 			log.Panic(err)
 		}
-	case "send":
-		err := sendCmd.Parse(os.Args[2:])
+	case "createwallet":
+		err := createWalletCmd.Parse(os.Args[2:])
 		if err != nil {
 			log.Panic(err)
 		}
+	case "getbalance":
+		err := getBalanceCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "listaddresses":
+		err := listAddressesCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}	
 	case "printchain":
 		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "send":
+		err := sendCmd.Parse(os.Args[2:])
 		if err != nil {
 			log.Panic(err)
 		}
@@ -112,7 +81,10 @@ func (cli *CLI) Run() {
 			createBlockchainCmd.Usage()
 			os.Exit(1)
 		}
-		cli.createBlockChain(*createBlockchainAddress)
+		cli.createBlockchain(*createBlockchainAddress)
+	}
+	if createWalletCmd.Parsed() {
+		cli.createWallet()
 	}
 	if getBalanceCmd.Parsed() {
 		if *getBalanceAddress == "" {
@@ -121,15 +93,18 @@ func (cli *CLI) Run() {
 		}
 		cli.getBalance(*getBalanceAddress)
 	}
+	if listAddressesCmd.Parsed(){
+		cli.listAddresses()
+	}
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
 	if sendCmd.Parsed() {
 		if *sendFrom == "" || *sendTo == "" || *sendAmount <= 0 {
 			sendCmd.Usage()
 			os.Exit(1)
 		}
 		cli.send(*sendFrom, *sendTo, *sendAmount)
-	}
-	if printChainCmd.Parsed() {
-		cli.printChain()
 	}
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gomxe
 
 go 1.13
 
-require github.com/boltdb/bolt v1.3.1
+require (
+	github.com/boltdb/bolt v1.3.1
+	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915
+)
